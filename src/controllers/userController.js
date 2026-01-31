4d8837393b535fe21902601d54dd96bc1aaf8bca
Masud <wubetumasud@gmail.com>
2026-01-31 17:54:47 +0300

    userRegistration and userRouter

---
+++ src/controllers/userController.js
@@ -0,0 +1,42 @@
+const asyncHandler = require('express-async-handler');
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/User');
+
+//@desc - register a new user
+//@route - POST /api/users/register
+
+const registerUser = asyncHandler( async (req, res) => {
+    // Get the payload
+    const { name, email, password } = req.body;
+
+    // Check if the user already exists
+    const userExists = await User.findOne({ email });
+    if(userExists) {
+        res.status(StatusCodes.BAD_REQUEST);
+        throw new Error('User already exists')
+    }
+
+    // Create new user
+    const user = await User.create({
+        name,
+        email,
+        password,    
+    });
+
+    if(user) {
+        res.status(StatusCodes.CREATED).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+            isAdmin: user.isAdmin,
+            profilePicture: user.profilePicture,
+        });
+    }else {
+        res.status(StatusCodes.BAD_REQUEST);
+    }
+});
+
+module.exports = {
+    registerUser,
+}
--- src/server.js
+++ src/server.js
@@ -16,8 +16,14 @@ mongoose
     })
     .catch((err) => {
         console.log('Error Connecting to database', err.message)
-    })
+    });
 
+// pass incoming data
+app.use(express.json());
+// Routes
+app.use('/api/users', userRouter);
+
+//Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log('Server is Running on port', PORT)
