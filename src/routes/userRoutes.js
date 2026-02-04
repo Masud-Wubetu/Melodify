aeaeac3b4045b6df4b1ff8336bfcbebfbafb0c28
Masud <wubetumasud@gmail.com>
2026-02-04 08:44:36 +0300

    Configure multer for file upload

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const userRouter = express.Router();
 const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/userController');
-const { protect } = require('../middlewares/auth')
+const { protect } = require('../middlewares/auth');
+const { upload } = require('../middlewares/upload')
 
 // Public routes
 userRouter.post('/register', registerUser );
@@ -9,7 +10,7 @@ userRouter.post('/login', loginUser);
 
 // Private routes
 userRouter.get('/profile', protect, getUserProfile);
-userRouter.put('/profile', protect, updateUserProfile);
+userRouter.put('/profile', protect, upload.single('profilePicture'), updateUserProfile);
 
 
 module.exports = userRouter;
