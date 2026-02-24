2b3f4bfb84aaa23dccce13ddf59570ff0ee620b3
Masud <wubetumasud@gmail.com>
2026-02-24 12:48:40 +0300

    Add Browse Podcasts element And style

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -9,12 +9,24 @@ export default function Sidebar() {
         </div>
       </div>
 
-      <div class="w-auto h-48 p-6 rounded-2xl bg-[#292626] mt-24">
+      <div class="w-auto h-48 p-6 rounded-2xl bg-[#292626] mt-24 border border-gray-300 hover:border-blue-500 transition">
         <p class="font-bold">Create your playlist first</p>
         <p class="mt-5">It's easy we will guide you</p>
-        <div class=" w-36 p-1 bg-white mt-7 rounded-3xl">
-          <p class="text-black text-center font-bold">Create playlist</p>
-        </div>
+        <button class="text-black px-2.5 py-2 hover:bg-blue-700 active:scale-95 
+                       transition duration-150 w-36 p-1 bg-white mt-7 rounded-3xl
+                       hover:scale-105 transition duration-300">
+          Create playlist
+        </button>
+      </div>
+
+      <div class="w-auto h-48 p-6 rounded-2xl bg-[#292626] mt-24 border border-gray-300 hover:border-blue-500 transition">
+        <p class="font-bold">Explore podcasts and start listening!</p>
+        <p class="mt-5">Stay tuned for updates on new episodes</p>
+        <button class="text-black px-2.5 py-2 hover:bg-blue-700 active:scale-95 
+                       transition duration-150 w-36 p-1 bg-white mt-7 rounded-3xl
+                       hover:scale-105 transition duration-300">
+          Browse podcasts
+        </button>
       </div>
 
     </aside>
